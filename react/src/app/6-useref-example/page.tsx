ca9aaafbfc7228159beaa6574ca2c01202a998a2
Alireza Talebizadeh <Alireza.talebizadeh78@gmail.com>
2024-04-09 08:15:30 +0330

    learn excude

--- react/src/app/6-useref-example/page.tsx
+++ react/src/app/6-useref-example/page.tsx
@@ -2,11 +2,26 @@
 import React, { useEffect, useRef } from "react";
 
 const UseRefExample = () => {
- 
+  const inputRef = useRef<HTMLInputElement>(null)
+  const userNameInputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    inputRef.current?.focus()
+  }, [])
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    console.log('user name is :' + userNameInputRef.current?.value);
+
+  }
+
+
+
+
+
   return (
     <div className="useRefExample">
       <input ref={inputRef} type="text" placeholder="focus here" />
-      <input ref={usernameInputRef} type="text" placeholder="username" />
+      <input ref={userNameInputRef} type="text" placeholder="username" />
       <button onClick={handleClick}>Send</button>
     </div>
   );
